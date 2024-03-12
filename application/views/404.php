19a898f8c40b73ba09b1b4645846aa14cc4fc646
Kousalya83 <105626326+Kousalya83@users.noreply.github.com>
2024-03-12 14:10:49 +0530

    Delete application directory

--- application/views/404.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-    var website = "<?= base_url(''); ?>";
-    alert("Something went wrong! Requested page is not found.");
-    window.location.href = website;
-</script>
